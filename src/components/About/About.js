514823a1c0f1a9f16e521739c1c5281ae228c435
vijay <mail2vijaynath@gmail.com                git config --global user.name mail2vijaynath@gmail.com>
2022-02-20 19:10:59 +0100

    updated resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import { about } from "../../portfolio";
-import pdf from "../../Resume/VivekGanesanInteriorDesignerResume.PDF";
+import pdf from "../../Resume/vivek resume updated.pdf";
 import "./About.css";
 
 const About = () => {
